f06274572496013810d3f725d95dc1a2c8f39125
frank.gen <frankgenerated@gmail.com>
2016-03-19 17:55:09 -0400

    Merged keymap files, updated configure file

--- configure.sh
+++ configure.sh
@@ -1,4 +1,5 @@
 rm ~/.i3/*
 ln -sfr ./i3/config ~/.i3/config
 ln -sfr ./i3/i3status.conf ~/.i3/i3status.conf
-ln -f ./sublime/user.sublime-keymap "~/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap"
+ln -f sublime/user.sublime-keymap "/home/frank/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap"
+ln -f sublime/Preferences.sublime-settings /home/frank/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
