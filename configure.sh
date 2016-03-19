c2b40e091fefbcab96c7684d5f105403185de939
frank.gen <frankgenerated@gmail.com>
2016-03-19 17:40:59 -0400

    added sublime ctrl-/ commands

--- configure.sh
+++ configure.sh
@@ -1,3 +1,4 @@
 rm ~/.i3/*
 ln -sfr ./i3/config ~/.i3/config
 ln -sfr ./i3/i3status.conf ~/.i3/i3status.conf
+ln -f ./sublime/user.sublime-keymap "~/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap"
